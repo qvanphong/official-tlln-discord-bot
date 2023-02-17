6b00825121b9cc5e55e7f8a5a19a1f9b2332886e
qvanphong <qvanphong@gmail.com>
2023-02-17 23:37:37 +0700

    update: track more CEX, add more symbol

--- helpers/dora_service.py
+++ helpers/dora_service.py
@@ -13,3 +13,19 @@ def get_wallet_balance(address: str, symbol: str):
             if assets["symbol"] == symbol:
                 return assets["balance"]
     return None
+
+
+def get_wallet_balance(address: str):
+    url = "https://dora.coz.io/api/v1/neo3/mainnet/balance/" + address
+
+    response = requests.get(url=url)
+    if 200 <= response.status_code < 300:
+        results = {}
+
+        json_response = response.json()
+        for assets in json_response:
+            results[assets["symbol"]] = assets["balance"]
+
+        return results
+
+    return {}
--- tasks/cex_gas_balance_watcher.py
+++ tasks/cex_gas_balance_watcher.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 import helpers.dora_service as dora_service
 from helpers import price_alert_utils as util, app_config
 
+
 class CEXGasBalanceWatcher:
     # Discord Bot
     discord_bot: commands.Bot = None
@@ -14,6 +15,28 @@ class CEXGasBalanceWatcher:
             "address": "NUqLhf1p1vQyP2KJjMcEwmdEBPnbCGouVp",
             "previous_balance": None,
             "balance": None,
+            "symbol": "GAS"
+        },
+        {
+            "cex_name": "Binance",
+            "address": "NUqLhf1p1vQyP2KJjMcEwmdEBPnbCGouVp",
+            "previous_balance": None,
+            "balance": None,
+            "symbol": "NEO"
+        },
+        {
+            "cex_name": "OKX",
+            "address": "NgJLhoyNBZPaEYDMm5vJbZHzdec6aTBQXU",
+            "previous_balance": None,
+            "balance": None,
+            "symbol": "GAS"
+        },
+        {
+            "cex_name": "OKX",
+            "address": "NgJLhoyNBZPaEYDMm5vJbZHzdec6aTBQXU",
+            "previous_balance": None,
+            "balance": None,
+            "symbol": "NEO"
         }
     ]
 
@@ -26,15 +49,38 @@ class CEXGasBalanceWatcher:
         embed_message = Embed(color=Color.green(), description="CEX's GAS balance 1 hr changes")
         embed_message.set_author(name="CEX's GAS Balance", icon_url=util.get_coin_image("GAS"))
 
+        loaded_address = {}
+        has_header = False
+
         for cex_info in self.cex_list:
-            balance = dora_service.get_wallet_balance(cex_info["address"], self.gas_symbol)
+            wallet_address = cex_info["address"]
+            symbol = cex_info["symbol"]
 
-            cex_info["previous_balance"] = balance if cex_info["previous_balance"] is None else cex_info["balance"]
-            cex_info["balance"] = balance
+            # add to dict to prevent request again in same address, just use the existed one
+            if wallet_address not in loaded_address:
+                balance = dora_service.get_wallet_balance(cex_info["address"])
+                loaded_address[wallet_address] = balance
+            else:
+                balance = loaded_address[wallet_address]
 
-            embed_message.add_field(name="Exchange", value="**{}**".format(cex_info["cex_name"]), inline=True)
-            embed_message.add_field(name="From", value="{:,.2f} GAS".format(cex_info["previous_balance"]), inline=True)
-            embed_message.add_field(name="To", value="{:,.2f} GAS".format(balance), inline=True)
+            if symbol not in balance:
+                return
+
+            symbol_balance = balance[symbol]
+            cex_info["previous_balance"] = symbol_balance if cex_info["previous_balance"] is None else cex_info[
+                "balance"]
+            cex_info["balance"] = symbol_balance
+
+            embed_message.add_field(name="Exchange" if has_header is False else "",
+                                    value="**{}**".format(cex_info["cex_name"], symbol),
+                                    inline=True)
+            embed_message.add_field(name="From" if has_header is False else "",
+                                    value="{:,.2f} {}".format(cex_info["previous_balance"], symbol),
+                                    inline=True)
+            embed_message.add_field(name="To" if has_header is False else "",
+                                    value="{:,.2f} {}".format(symbol_balance, symbol),
+                                    inline=True)
+            has_header = True
 
         if first_init is False:
             coin_channel = util.get_channel_id("GAS")
@@ -43,7 +89,6 @@ class CEXGasBalanceWatcher:
                     .get_guild(app_config.get_config("server_id")) \
                     .get_channel(util.get_channel_id("GAS")) \
                     .send(embed=embed_message)
-
 
     def _create_and_write_balance_to_file(self, file_path, balance):
         f = open(file_path, "x")
